c118bbd3ce2d9adf33f48e8e8bdc1c4ea46717df
tomikode <tomi.piisila@gmail.com>
2022-07-29 19:59:29 +1000

    favicon, site title, styling

---
+++ components/Meta.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const Meta = () => {
+	return (
+		<Head>
+			<title>QuickType</title>
+			<meta
+				name="viewport"
+				content="initial-scale=1.0, width=device-width"
+			/>
+		</Head>
+	);
+};
+
+export default Meta;
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,22 @@ import TypingTest from "../components/Typingtest";
 import Gamefinish from "../components/Gamefinish";
 import Menubar from "../components/Menubar";
 import Footer from "../components/Footer";
-
+import Meta from "../components/Meta";
 
 export default function Home() {
-	const [stats, setStats] = useState({ wpm: -1, lpm: -1, time: 0, words: 0  });
-	const [wordNumber, setWordNumber] = useState(20)
-	const [reload, setReload] = useState(1)
+	const [stats, setStats] = useState({ wpm: -1, lpm: -1, time: 0, words: 0 });
+	const [wordNumber, setWordNumber] = useState(20);
+	const [reload, setReload] = useState(1);
 
 	const setWordsForGame = (num) => {
-		setReload(reload => reload + 1)
-		setWordNumber(num)
-		setStats({ wpm: -1, lpm: -1 })
-	}
+		setReload((reload) => reload + 1);
+		setWordNumber(num);
+		setStats({ wpm: -1, lpm: -1 });
+	};
 
 	const endGame = (wpm, lpm, time) => {
-		setStats({ wpm, lpm, time, words: wordNumber })
-	}
+		setStats({ wpm, lpm, time, words: wordNumber });
+	};
 
 	//icons for endgame
 	//footer
@@ -27,17 +27,32 @@ export default function Home() {
 	//deploy
 
 	return (
-		<div className="p-6 h-screen">
-			<div className="h-[10vh]">
-				<Menubar setWordsForGame={setWordsForGame} wordNumber={wordNumber} />
-			</div>
-			<div className="h-[81vh]">
-				{stats.wpm !== -1 ? <Gamefinish stats={stats} setWordsForGame={setWordsForGame} /> :
-					<TypingTest endGame={endGame} wordNumber={wordNumber} reload={reload} />
-				}
-			</div>
-			<div className="h-[6vh]">
-				<Footer />
+		<div>
+			<Meta />
+			<div className="p-6 h-screen">
+				<div className="h-[10vh]">
+					<Menubar
+						setWordsForGame={setWordsForGame}
+						wordNumber={wordNumber}
+					/>
+				</div>
+				<div className="h-[81vh]">
+					{stats.wpm !== -1 ? (
+						<Gamefinish
+							stats={stats}
+							setWordsForGame={setWordsForGame}
+						/>
+					) : (
+						<TypingTest
+							endGame={endGame}
+							wordNumber={wordNumber}
+							reload={reload}
+						/>
+					)}
+				</div>
+				<div className="h-[6vh]">
+					<Footer />
+				</div>
 			</div>
 		</div>
 	);
